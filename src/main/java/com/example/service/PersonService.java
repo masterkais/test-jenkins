a97031039eb1aa361537b57c85ca8d83a10640da
masterkais <kais2032@gmail.com>
2021-12-29 14:34:59 +0100

    testing webhook

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -15,8 +15,8 @@ public class PersonService {
 		Person p = new Person();
 		p.setId("1");
 		p.setAge(21);
-		p.setFirstName("Khaled");
-		p.setLastName("Amrosy");
+		p.setFirstName("kais");
+		p.setLastName("trabelsi");
 		persons.add(p);
 
 		p = new Person();
